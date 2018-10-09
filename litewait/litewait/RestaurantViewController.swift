33ca39f6b6a42dcd50b741b68632f8ce8f49cbd1
jch026 <jch026@ucsd.edu>
2018-10-09 10:08:29 -0700

    Added the Restaurant View Controller

---
+++ litewait/litewait/RestaurantViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantViewController.swift
+//  litewait
+//
+//  Created by Joseph Ho on 10/9/18.
+//  Copyright © 2018 dumplingNinjas. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
